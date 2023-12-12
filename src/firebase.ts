be9c0a370b27805db1bc596fa9f658a806bbbe45
강다연 <dksldiss89@gmail.com>
2023-12-12 16:45:43 +0900

    Create firebase project

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAWKIFGMT3BG_TVcNPD5NW6xyy5b3feX1w",
+    authDomain: "twitter-clone-5f4ac.firebaseapp.com",
+    projectId: "twitter-clone-5f4ac",
+    storageBucket: "twitter-clone-5f4ac.appspot.com",
+    messagingSenderId: "628214391782",
+    appId: "1:628214391782:web:0c348d0f08dde63d586c27"
+};
+
+const app = initializeApp(firebaseConfig);
