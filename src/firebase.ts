9b82a6acb07bfb215e2af5b1718e04a2fe752f2d
강다연 <dksldiss89@gmail.com>
2023-12-13 14:17:13 +0900

    Modify firebase setup

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAWKIFGMT3BG_TVcNPD5NW6xyy5b3feX1w",
@@ -10,3 +11,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
