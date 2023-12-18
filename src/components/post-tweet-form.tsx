c54a56eeb8a74a14c2c1246d0b21316654c32876
강다연 <dksldiss89@gmail.com>
2023-12-18 17:57:42 +0900

    Modify tweet submit function

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import {useState} from "react";
+import {addDoc, collection} from "firebase/firestore"
+import {auth, db} from "../firebase";
 
 const Form = styled.form`
     display: flex;
@@ -71,8 +73,28 @@ export default function PostTweetForm(){
             setFile(files[0])
         }
     }
+
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const user = auth.currentUser
+        if(!user || isLoading || tweet === "" || tweet.length > 180) return
+
+        try {
+            setIsLoading(true)
+            await addDoc(collection(db, "tweets"), {
+                tweet,
+                createdAt: Date.now(),
+                username: user.displayName || "Anonymous",
+                userId: user.uid
+            })
+        } catch (e){
+            console.log(e)
+        } finally {
+            setIsLoading(false)
+        }
+    }
     return(
-        <Form>
+        <Form onSubmit={onSubmit}>
             <TextArea rows={5} maxLength={180} onChange={onChange} value={tweet} placeholder="What is happening?" />
             <AttachFileButton htmlFor="file">{file? "Photo added ✅" : "Add Photo"}</AttachFileButton>
             <AttachFileInput onChange={onFileChange} type="file" id="file" accept="image/*" />
