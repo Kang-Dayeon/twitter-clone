7b88c76cce5c41dc17f6e58d692a6783211ca144
강다연 <dksldiss89@gmail.com>
2023-12-27 13:57:10 +0900

    Modify realtime tweet

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,6 +1,6 @@
 import {useEffect, useState} from "react";
 import styled from "styled-components";
-import {query, collection, orderBy, getDocs} from "firebase/firestore";
+import {query, collection, orderBy, getDocs, onSnapshot, Unsubscribe, limit} from "firebase/firestore";
 import {db} from "../firebase";
 import Tweet from "./tweet";
 
@@ -18,31 +18,50 @@ const Wrapper = styled.div``
 export default function Timeline(){
     const [tweets, setTweets] = useState<ITweet[]>([])
 
-    const fetchTweets = async () => {
-        const tweetsQuery = query(
-            collection(db, "tweets"),
-            orderBy("createdAt", "desc")
-        )
-
-        const snapshot = await getDocs(tweetsQuery)
-
-        const tweets = snapshot.docs.map((doc) => {
-            const {tweet, createdAt, userId, username, photo} = doc.data()
-            return {
-                tweet,
-                createdAt,
-                userId,
-                username,
-                photo,
-                id: doc.id,
-            }
-        })
-
-        setTweets(tweets)
-    }
-
     useEffect(() => {
+        let unsubscribe : Unsubscribe | null = null
+        const fetchTweets = async () => {
+            const tweetsQuery = query(
+                collection(db, "tweets"),
+                orderBy("createdAt", "desc"),
+                limit(25)
+            )
+
+            // const snapshot = await getDocs(tweetsQuery)
+            //
+            // const tweets = snapshot.docs.map((doc) => {
+            //     const {tweet, createdAt, userId, username, photo} = doc.data()
+            //     return {
+            //         tweet,
+            //         createdAt,
+            //         userId,
+            //         username,
+            //         photo,
+            //         id: doc.id,
+            //     }
+            // })
+
+            // 쿼리에 리스너를 붙여준것
+            unsubscribe = await onSnapshot(tweetsQuery, (snapshot) => {
+                const tweets = snapshot.docs.map((doc) => {
+                    const {tweet, createdAt, userId, username, photo} = doc.data()
+                    return {
+                        tweet,
+                        createdAt,
+                        userId,
+                        username,
+                        photo,
+                        id: doc.id,
+                    }
+                })
+                setTweets(tweets)
+            })
+        }
         fetchTweets()
+
+        return () => {
+            unsubscribe && unsubscribe()
+        }
     },[])
     return(
         <Wrapper>
