700a63556e34b7e38d5f8103d8f9bc07467a84eb
강다연 <dksldiss89@gmail.com>
2024-01-02 16:06:59 +0900

    Modify menu style

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -49,13 +49,26 @@ const MenuItem = styled.div`
         right: 15%;
     }
     &:hover {
-        bottom: 15px;
+        bottom: 30px;
         background: #fff;
         svg {
             width: 33px;
             stroke: #17212d;
         }
+        .menu-name{
+            font-size: 14px;
+            bottom: -18px;
+        }
     }
+`
+
+const MenuName = styled.span`
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    font-size: 0;
+    color: #fff;
 `
 
 export default function Layout(){
@@ -76,6 +89,7 @@ export default function Layout(){
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" className="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"/>
                         </svg>
+                        <MenuName className="menu-name">Home</MenuName>
                     </MenuItem>
                 </Link>
                 <Link to="/Profile">
@@ -83,14 +97,14 @@ export default function Layout(){
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" className="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"/>
                         </svg>
-
+                        <MenuName className="menu-name">Profile</MenuName>
                     </MenuItem>
                 </Link>
                 <MenuItem onClick={onLogout} className="log-out">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" className="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15"/>
                     </svg>
-
+                    <MenuName className="menu-name">Logout</MenuName>
                 </MenuItem>
             </Menu>
             {/* router를 활용해서 layout 밑에 자식들이 여기로 들어옴 */}
