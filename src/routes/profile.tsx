09ffabb3d65882f275714d05344248779fb3c1c2
강다연 <dksldiss89@gmail.com>
2024-01-03 15:39:14 +0900

    Modify edit usrname

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -58,11 +58,47 @@ const Tweets = styled.div`
     }
 `
 
+const UserInfo = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+const EditName = styled.button`
+    padding: 0;
+    margin-left: 6px;
+    border: none;
+    background: transparent;
+    width: 18px;
+    height: 18px;
+    cursor: pointer;
+    svg {
+        width: 18px;
+    }
+    &.submit{
+    width: 30px;
+    height: 30px;
+    svg {
+        width: 30px;
+    }
+    }
+`
+
+const NameInput = styled.input`
+    padding: 7px 15px;
+    background: #fff;
+    border: none;
+    border-radius: 20px;
+    font-size: 16px;
+    width: 180px;
+`
+
 export default function Profile(){
     const user = auth.currentUser
 
     const [avatar, setAvatar] = useState(user?.photoURL)
     const [tweets, setTweets] = useState<ITweet[]>([])
+    const [edit, setEdit] = useState(false)
+    const [name, setName] = useState(user.displayName)
 
     const onAvatarChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
         const {files} = e.target
@@ -101,6 +137,30 @@ export default function Profile(){
         setTweets(tweets)
     }
 
+    const onClickNameChange = () => {
+        setEdit(true)
+    }
+
+    const onNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setName(e.target.value)
+    }
+
+    const onSubmitName = async () => {
+        if(!user) return
+        try {
+            if(name.length <= 0){
+                alert("Please write your name")
+            } else {
+                await updateProfile(user, {
+                    displayName: name,
+                })
+                setEdit(false)
+            }
+        } catch (e){
+            console.log(e)
+        }
+    }
+
     useEffect(() => {
         fetchTweets()
     }, [])
@@ -118,9 +178,27 @@ export default function Profile(){
                 </AvatarUpload>
                 <AvatarInput onChange={onAvatarChange} type="file" accept="image/*" id="avatar" />
             </AvatarWrap>
-            <Name>
-                {user?.displayName ?? "Anonymous"}
-            </Name>
+            {edit ? (
+                <UserInfo>
+                    <NameInput onChange={onNameChange} type="text" value={name} />
+                    <EditName className="submit" onClick={onSubmitName}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" className="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                        </svg>
+                    </EditName>
+                </UserInfo>
+            ) : (
+                <UserInfo>
+                    <Name>
+                        {user?.displayName ?? "Anonymous"}
+                    </Name>
+                    <EditName onClick={onClickNameChange}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" className="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"/>
+                        </svg>
+                    </EditName>
+                </UserInfo>
+            )}
             <Tweets>
                 {tweets.map(tweet => (
                     <Tweet key={tweet.id} {...tweet} />
